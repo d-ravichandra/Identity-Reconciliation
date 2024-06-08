9494a4448876aed886fd34962c7dac5d7066f825
RaviChandraCS <ravichandraduvvuri1999@gmail.com>
2024-06-08 19:54:33 +0530

    postgres

--- DBController/config.js
+++ DBController/config.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('bitespeed', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false,
+// const sequelize = new Sequelize('bitespeed', 'root', 'root', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     logging: false,
+// });
+
+const db = 'postgres://users_mwqc_user:yB6XP2JlpEf3eJjsZOqCZcb3dRhF5NOo@dpg-cpi62csf7o1s73bcn2c0-a/users_mwqc';
+
+const sequelize = new Sequelize(db, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false
 });
 
 module.exports = sequelize;
